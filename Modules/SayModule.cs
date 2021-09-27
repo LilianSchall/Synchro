0d2e538530939cf5ab25a0a9400c13e176bb47b1
Lilian Schall <lilian.schall@epita.fr>
2021-09-27 17:59:54 +0200

    added setting module, ping module that can be used to check if the bot is only or not (admin only)

--- Modules/SayModule.cs
+++ Modules/SayModule.cs
@@ -1,4 +1,19 @@
-$HEADER$namespace $NAMESPACE$
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace Synchro.Modules
 {
-  public class $CLASS$ {$END$}
+    public class SayModule : SettingsBase
+    {
+        /// <summary>
+        /// Simple ping pong command that is used so we can know if the bot is online
+        /// </summary>
+        [Command("ping")]
+        public async Task Ping()
+        {
+            await ReplyAsync($"🏓 Pong {Context.User.Mention}!");
+        }
+    }
 }
--- Modules/SettingsModule.cs
+++ Modules/SettingsModule.cs
@@ -1,4 +1,28 @@
-$HEADER$namespace $NAMESPACE$
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace Synchro.Modules
 {
-  public class $CLASS$ {$END$}
+    [Group("settings")]
+    [RequireUserPermission(GuildPermission.Administrator)]
+    public class SettingsBase : ModuleBase<SocketCommandContext>
+    {
+        /// <summary>
+        /// Default action, displays every subcommand of settings section
+        /// </summary>
+        [Command]
+        public async Task DefaultAction()
+        {
+            Embed embed = new EmbedBuilder()
+            {
+                Title = "Settings",
+                Color = Color.DarkBlue,
+                Description =
+                    "🏓 Ping: \t Connection test ! Try it !. \n\n"
+
+            }.Build();
+            await ReplyAsync(embed: embed);
+        }
+    }
 }
