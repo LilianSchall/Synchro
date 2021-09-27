a55e78cf22252da253cebba2dd6d6b33fc67984e
Lilian Schall <lilian.schall@epita.fr>
2021-09-27 18:04:53 +0200

    Added Voice Module that integrates a command making the bot leave it's current voice channel when the user calling it is in the same channel as the bot

---
+++ Modules/VoiceModule.cs
@@ -0,0 +1,35 @@
+using System.Threading.Tasks;
+using Discord;
+using Discord.Audio;
+using Discord.Commands;
+using Synchro.Models;
+using Synchro.Services;
+
+namespace Synchro.Modules
+{
+    public class VoiceModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("leave")]
+        public async Task Leave()
+        {
+            IVoiceChannel channel = (Context.User as IGuildUser)?.VoiceChannel;
+            if (channel == null)
+            {
+                await ReplyAsync("❌ **You are not connected to any voice channel currently.**");
+                return;
+            }
+            if (channel != Context.Guild.CurrentUser.VoiceChannel)
+            {
+                await ReplyAsync(
+                    "❌ **Cannot leave voice channel, you are not in the channel **" + Context.Guild.CurrentUser.VoiceChannel);
+                return;
+            }
+
+            await ReplyAsync("✅ **Disconnected from channel: **" + channel.Name);
+            BotProperties.GuildPropsMap[channel.GuildId].ClearMusicProvider();
+            
+            await Context.Guild.CurrentUser.VoiceChannel.DisconnectAsync();
+        }
+        
+    }
+}
