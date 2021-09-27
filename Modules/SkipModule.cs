253641cfd95bf3c6545dbc8c8b0a7f8c4b255de4
Lilian Schall <lilian.schall@epita.fr>
2021-09-27 18:05:31 +0200

    Added Skip Module that integrate a command used to skip the current played music

---
+++ Modules/SkipModule.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Audio;
+using Discord.Net;
+using Discord.Commands;
+using Discord.WebSocket;
+using Synchro.Models;
+using Synchro.Services;
+
+namespace Synchro.Modules
+{
+    public class SkipModule : ModuleBase<SocketCommandContext>
+    {
+
+        [Command("skip")]
+        public async Task Skip()
+        {
+            await ReplyAsync("⏩ **Skipping to next music...**");
+            Console.WriteLine("Skip module called");
+            await BotProperties.GuildPropsMap[Context.Guild.Id].SkipToNextMusic();
+            Console.WriteLine("Skip module finished");
+        }
+    }
+}
