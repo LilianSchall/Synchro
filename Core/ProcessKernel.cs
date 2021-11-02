1878e853e3995ea4b25f60eb14ab9b4ec9b82cc7
Lilian Schall <lilian.schall@epita.fr>
2021-11-02 22:48:39 +0100

    Fixing token

--- Core/ProcessKernel.cs
+++ Core/ProcessKernel.cs
@@ -32,7 +32,7 @@ namespace Synchro.Core
             if (!Debugger.IsAttached)
                 _discordToken = Environment.GetEnvironmentVariable("DISCORDTOKEN");
             else
-                _discordToken = "***REMOVED***";
+                _discordToken = "";
             Console.WriteLine("discord token: " + _discordToken);
             if (_discordToken == null)
             {
